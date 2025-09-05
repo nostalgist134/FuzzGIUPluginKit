4555e0bc880b04958eea0ef72595a979f3c48afe
nostalgist134 <CAAA3333@126.com>
2025-09-05 22:35:44 +0800

    update, merge go plugin templates

--- cmd/common/plugInfoGoPlugin.go
+++ cmd/common/plugInfoGoPlugin.go
@@ -14,22 +14,27 @@ func GetPluginInfo(pluginFile string) (*convention.PluginInfo, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	piSym, err := p.Lookup("PluginInfo")
 	if err != nil {
 		return nil, err
 	}
+
 	pluginInfoFun, ok := piSym.(func() string)
 	if !ok {
 		return nil, errors.New("PluginInfo is not func() string")
 	}
+
 	pi := pluginInfoFun()
 	if pi == "" {
 		return nil, errors.New("PluginInfo returned empty")
 	}
+
 	ret := new(convention.PluginInfo)
 	err = json.Unmarshal([]byte(pi), ret)
 	if err != nil {
 		return nil, err
 	}
+
 	return ret, nil
 }
